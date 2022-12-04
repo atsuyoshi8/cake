450669bbce301b573bf6258e54085b41c0c3f311
EC2 Default User <ec2-user@ip-172-31-16-91.ap-northeast-1.compute.internal>
2022-12-04 08:46:17 +0000

    2022-12-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,36 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # 会員側のルーティング設定
   scope module: :public do
-    get 'items' => 'items#index'
+    root to: "homes#top"
+    get 'homes/about' => 'homes#about',as:'about'
+    resources:items,only:[:index,:show]
+    resources:customers,only:[:index,:edit,:create,:update,:destroy]
+    resources:cart_items,only:[:index,:update,:destroy,:destroy_all,:create]
+    resources:orders,only:[:new,:confilm,:complete,:create,:index,:show]
+    resources:addresses,only:[:index,:edit,:create,:update,:destroy]
   end
   # 管理者側のルーティング設定
   namespace :admin do
-    resources :items
+    root to: "homes#top"
+    get 'homes/about' => 'homes#about',as:'about'
+    resources :items,only:[:index,:new,:create,:show,:edit,:update]
+    resources :genres,only:[:index,:create,:edit,:update]
+    resources :customers,only:[:index,:show,:edit,:update]
+    resources :orders,only:[:show,:update]
+    resources :order_details,only:[:update]
   end
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_26_062433) do
+ActiveRecord::Schema.define(version: 2022_12_04_045514) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -22,10 +22,15 @@ ActiveRecord::Schema.define(version: 2022_11_26_062433) do
   end
 
   create_table "admins", force: :cascade do |t|
-    t.string "email"
-    t.string "encrypted_password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "cart_items", force: :cascade do |t|
@@ -37,6 +42,8 @@ ActiveRecord::Schema.define(version: 2022_11_26_062433) do
   end
 
   create_table "customers", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -49,7 +56,7 @@ ActiveRecord::Schema.define(version: 2022_11_26_062433) do
     t.string "postal_code"
     t.string "address"
     t.string "telephone_number"
-    t.boolean "is_deleted", default: false, null: false
+    t.boolean "is_deleted"
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
